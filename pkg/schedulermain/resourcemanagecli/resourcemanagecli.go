65044af0f522bb59abf3de22b80bf3df66acbce8
Azipro <532789173@qq.com>
2022-07-18 19:35:17 +0800

    some hints

--- pkg/schedulermain/resourcemanagecli/resourcemanagecli.go
+++ pkg/schedulermain/resourcemanagecli/resourcemanagecli.go
@@ -1,1 +1,23 @@
 package resourcemanagecli
+
+import (
+	"log"
+	"net/rpc"
+
+	"github.com/AgentGuo/scheduler/pkg/resourcemanage/apis"
+)
+
+func call(rpcName string, args interface{}, reply interface{}) error {
+	client, err := rpc.Dial("tcp", "localhost:12351")
+	if err != nil {
+		log.Println("dial error:", err)
+		return err
+	}
+	err = client.Call(apis.ResourceManageRPC+"."+rpcName, args, reply)
+	if err != nil {
+		log.Println("call error:", err)
+		return err
+	}
+	client.Close()
+	return nil
+}
